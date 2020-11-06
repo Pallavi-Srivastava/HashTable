d51e93dc7fe227c6ddff571b04bf261a4952b9e6
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-06 18:54:53 +0530

    Added:Code for remove frequency of words from large paragraph

---
+++ src/test/java/com/blz/hashtabletest/MyHashMapTest.java
@@ -0,0 +1,63 @@
+package com.blz.hashtabletest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.blz.hashtable.MyHashMap;
+import com.blz.hashtable.MyLinkedHashMap;
+
+public class MyHashMapTest {
+
+	@Test
+	public void givenASentenceWhenWordsAreAddedToListShouldReturnWordFrequency() {
+		String sentence = "To be or not to be";
+		MyHashMap<String, Integer> myHashMap = new MyHashMap<String, Integer>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myHashMap.add(word, value);
+		}
+		int frequency = myHashMap.get("to");
+		assertEquals(2, frequency);
+	}
+
+	@Test
+	public void givenASentenceWhenWordsAreAddedShouldReturnWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		assertEquals(3, frequency);
+	}
+
+	@Test
+	public void givenASentence_WhenWordsAreRemoved_ShouldReturnRemovedWord() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately "
+						   + "into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		myLinkedHashMap.remove("avoidable");
+		Integer frequency = myLinkedHashMap.get("avoidable");
+		assertEquals(null, frequency);
+	}
+}
